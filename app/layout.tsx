71bcfb4b3169b84ba78345631617282f9232f63d
illuzion <91480011+illuzionm@users.noreply.github.com>
2025-07-07 22:40:19 +0700

    🤬

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
 }
 export const viewport: Viewport = {
   themeColor: '#fff',
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover',
 }
 
 export default function RootLayout({
